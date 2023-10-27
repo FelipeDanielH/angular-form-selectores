eb8e54e0ea26fc1cf77e7c3e750e1fb5a0920eaa
Felipe <felipe.daniel.henriquez@gmail.com>
2023-10-27 19:39:30 -0300

    feat(global): cargar estructura de la aplicacion y primer formulario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
